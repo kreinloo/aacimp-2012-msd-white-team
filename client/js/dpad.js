d0a8585d1a5b739165b119da42221066f4674d49
naubktl <naubktl@gmail.com>
2012-08-08 12:41:03 +0300

    by Bakai

--- client/js/dpad.js
+++ client/js/dpad.js
@@ -1,30 +1,19 @@
-var height = screen.height;
-var width = screen.width;
+jQuery(document).bind('pageinit',function(){
 
-arrowUp = document.getElementById("arrowUp");
-arrowDown = document.getElementById("arrowDown");
-arrowRight = document.getElementById("arrowRight");
-arrowLeft = document.getElementById("arrowLeft");
-buttonShoot = document.getElementById("buttonShoot");
+    $("#arrowUp").tap(function(){
+        alert("UP");
+    });
+    $("#arrowDown").tap(function(){
+        alert("DOWN");
+    });
+    $("#arrowLeft").tap(function(){
+        alert("LEFT");
+    });
+    $("#arrowRight").tap(function(){
+        alert("RIGHT");
+    });
+    $("#buttonShoot").tap(function(){
+        alert("BOOM");
+    });
 
-
-function touchUp(){
-    alert("UP");
-}
-function touchDown(){
-    alert("DOWN");
-}
-function touchRight(){
-    alert("RIGHT");
-}
-function touchLeft(){
-    alert("LEFT");
-}
-function touchShoot(){
-    alert("BOOOM!");
-}
-arrowUp.addEventListener("touchstart", touchUp, false);
-arrowDown.addEventListener("touchstart", touchDown, false);
-arrowRight.addEventListener("touchstart", touchRight, false);
-arrowLeft.addEventListener("touchstart", touchLeft, false);
-buttonShoot.addEventListener("touchstart", touchShoot, false);
+});
