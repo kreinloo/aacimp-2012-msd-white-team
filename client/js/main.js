e1f855cb55d6927103447641117b9d66ef2e1597
naubktl <naubktl@gmail.com>
2012-08-11 15:18:08 +0300

    by Bakai

--- client/js/main.js
+++ client/js/main.js
@@ -160,7 +160,7 @@ $(function () {
         : {acc: e.accY, actions: ['moveDown', 'moveUp']};
 
       if (Math.abs(params.acc) > accActivationLevel) {
-        var dir = params.acc > 0 ? 0 : 1;
+        var dir = params.acc > 0 ? 1 : 0;
         var action = params.actions[dir];
         player.stop();
         player[action]();
